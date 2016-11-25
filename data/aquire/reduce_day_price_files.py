e33bb057ff57c9ada11e276b357138e58150168c
mcstar <mcstar@gmail.com>
2016-11-24 21:12:25 -0600

    reduce day price outputs to stdout now

--- data/aquire/reduce_day_price_files.py
+++ data/aquire/reduce_day_price_files.py
@@ -22,28 +22,23 @@ def process_lines():
     current_day = end = None
     ts = TimeStamp()
     file_lines = []
-
+    d ={}
     for price_line in sys.stdin:
         
         data = price_line.split(',')
         date = ts.todate(float(data[0])/1000)
+        dateTs = ts.totimestamp(date)
         price = float(data[1])
-        if not current_day:
-            current_day = datetime(date.year,date.month,date.day)
-            end = current_day + timedelta(days=1)  
-        else:
-            if date >= end:
-                if len(file_lines) > 10:
-                    save_to("%s.csv" % str(ts.totimestamp(current_day)),file_lines)
-                del file_lines 
-                file_lines=[]           
-                # set the next file date
-                current_day = datetime(date.year,date.month,date.day) 
-                end = current_day + timedelta(days=1)
-        file_lines.append("%s,%s" % (str(ts.totimestamp(date)),str(price)))
-    if len(file_lines) > 10:
-        save_to("%s.csv" % str(ts.totimestamp(current_day)),file_lines)
-    del file_lines
+        day = datetime(date.year,date.month,date.day)
+        dayTs = ts.totimestamp(day)
+        if not dayTs in d:
+            d[dayTs] = {}
+        if not dateTs in d[dayTs]:
+            d[dayTs][dateTs] = price
+    # write the results
+    for currentDayKey,dayDict in d:
+        for currentTimeKey,price in dayDict:
+            print "%s,%s,%s" % (str(currentDayKey),str(currentTimeKey),str(price))
 
 if __name__ == "__main__":
     process_lines()
---
+++ start-container.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# the default node number is 3
+N=${1:-3}
+
+
+# start hadoop master container
+sudo docker rm -f hadoop-master &> /dev/null
+echo "start hadoop-master container..."
+sudo docker run -itd \
+                --net=hadoop \
+                -p 50070:50070 \
+                -p 8088:8088 \
+		-v /hdfs/master:/root/hdfs \
+                --name hadoop-master \
+                --hostname hadoop-master \
+                kiwenlau/hadoop:1.1 &> /dev/null
+
+		#-v /hdfs/master:/root/hdfs \
+
+# start hadoop slave container
+i=1
+while [ $i -lt $N ]
+do
+	sudo docker rm -f hadoop-slave$i &> /dev/null
+	echo "start hadoop-slave$i container..."
+	sudo docker run -itd \
+	                --net=hadoop \
+			-v /hdfs/slave$i:/root/hdfs \
+			-p $((19887+$i)):19888 \
+			-p $((8041+$i)):19888 \
+	                --name hadoop-slave$i \
+	                --hostname hadoop-slave$i \
+	                kiwenlau/hadoop:1.1
+# &> /dev/null
+	i=$(( $i + 1 ))
+done 
+
+#-v /hdfs/slave$i:/root/hdfs \
+# get into hadoop master container
+sudo docker exec -it hadoop-master bash
