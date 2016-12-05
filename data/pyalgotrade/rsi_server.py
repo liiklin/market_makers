1d15740266599011ae047d8c55a03f3e4495bb8b
mcstar <mcstar@gmail.com>
2016-12-05 08:07:11 -0600

    rsi_server to use btc_all_5_min

--- data/pyalgotrade/rsi_server.py
+++ data/pyalgotrade/rsi_server.py
@@ -6,18 +6,18 @@ from pyalgotrade.optimizer import server
 
 def parameters_generator():
     instrument = ["btc"]
-    entrySMA = range(150, 251)
-    exitSMA = range(5, 16)
-    rsiPeriod = range(2, 11)
+    entrySMA = range(99, 15)
+    exitSMA = range(8, 16)
+    rsiPeriod = range(2, 4)
     overBoughtThreshold = range(75, 96)
-    overSoldThreshold = range(5, 26)
+    overSoldThreshold = range(16, 26)
     return itertools.product(instrument, entrySMA, exitSMA, rsiPeriod, overBoughtThreshold, overSoldThreshold)
 
 # The if __name__ == '__main__' part is necessary if running on Windows.
 if __name__ == '__main__':
     # Load the feed from the CSV files.
     feed = yahoofeed.Feed()
-    feed.addBarsFromCSV("btc", "btc_all_daily.csv")
+    feed.addBarsFromCSV("btc", "btc_all_5_min.csv")
 
     # Run the server.
     server.serve(feed, parameters_generator(), "localhost", 5000)
