823fb8e5879fddfd1c8bd3baf1bc2b6cea865a5d
michael schwab <mcstar@gmail.com>
2016-12-07 08:35:06 -0600

    ip address of server added

--- data/pyalgotrade/rsi2_worker.py
+++ data/pyalgotrade/rsi2_worker.py
@@ -4,4 +4,4 @@ import rsi2
 
 # The if __name__ == '__main__' part is necessary if running on Windows.
 if __name__ == '__main__':
-    worker.run(rsi2.RSI2, "localhost", 5000, workerName="localworker")
+    worker.run(rsi2.RSI2, "192.168.1.10", 5000, workerName="localworker")
