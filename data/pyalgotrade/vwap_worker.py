8af68b313b7f2adfb8f05e8597d906cc4c0877b3
mcstar <mcstar@gmail.com>
2016-12-07 15:35:11 -0600

    Vwap strategy implemented for BTC

--- data/pyalgotrade/vwap_worker.py
+++ data/pyalgotrade/vwap_worker.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 from pyalgotrade.optimizer import worker
-import rsi2
+import vwap_strategy
 
 # The if __name__ == '__main__' part is necessary if running on Windows.
 if __name__ == '__main__':
-    worker.run(rsi2.RSI2, "192.168.1.10", 5000, workerName="localworker")
+    worker.run(vwap_strategy.VWAPMomentum,"192.168.1.10", 5001, workerName="localworker")
 
