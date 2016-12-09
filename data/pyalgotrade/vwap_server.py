d5d7dc47e26711d2051a9a9b76a5c4e15f032174
mcstar <mcstar@gmail.com>
2016-12-08 22:19:15 -0600

    30mine updates and strategy updates

--- data/pyalgotrade/vwap_server.py
+++ data/pyalgotrade/vwap_server.py
@@ -3,23 +3,28 @@ import itertools
 from pyalgotrade.optimizer import server
 from pyalgotrade.barfeed import csvfeed
 from pyalgotrade import bar
-
+lenparams = 0
 def parameters_generator():
     instrument = ["BTC"]
     initialCash= [1000]
-    vwapWindowSize = range(90,250,5)
-    buyThreshold = map(lambda x : float(x)/1000 , range(1,20,1))
-    sellThreshold = map(lambda x : float(x)/1000 , range(1,20,1))
-    return itertools.product(instrument, initialCash,vwapWindowSize,buyThreshold,sellThreshold)
+    vwapWindowSize = range(89,180,1)
+    buyThreshold = map(lambda x : float(x)/1000 , range(5,40,1))
+    sellThreshold = map(lambda x : float(x)/1000 , range(5,40,1))
+    lenparams = len(sellThreshold) * len(buyThreshold) * len(vwapWindowSize)
+    return (lenparams, itertools.product(instrument, initialCash,vwapWindowSize,buyThreshold,sellThreshold))
 
 # The if __name__ == '__main__' part is necessary if running on Windows.
 if __name__ == '__main__':
     instrument = "BTC"
     year = "2016"
+    minutes = 30
+    
 	# Load the feed from the CSV files.
     barFeed = csvfeed.GenericBarFeed(bar.Frequency.MINUTE*30)
     
-    barFeed.addBarsFromCSV(instrument, "30min-%s-%s.csv" % (instrument,year))
+    barFeed.addBarsFromCSV(instrument, "%smin-%s-%s.csv" % (minutes,instrument,year))
   
     # Run the server.
-    server.serve(barFeed, parameters_generator(), "0.0.0.0", 5001)
+    lenp, params = parameters_generator()
+    print "Running %s variations" % lenp
+    server.serve(barFeed,params, "0.0.0.0", 5001)
