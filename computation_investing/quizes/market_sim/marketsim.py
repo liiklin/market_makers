23c815d70f03909f9d3b072ccbf2432d1748ff48
Michael Schwab <mike@mdmetrix.com>
2018-02-25 08:04:49 -0600

    Market Sim

--- computation_investing/quizes/market_sim/marketsim.py
+++ computation_investing/quizes/market_sim/marketsim.py
@@ -1,18 +1,57 @@
 import click
 import QSTK.qstkutil.qsdateutil as du
 import QSTK.qstkutil.DataAccess as da
-from simluate import *
-@click.command()
-@click.argument("initial_amount")
-@click.argument("orders_file", type=click.Path(exists=True))
-@click.argument("out_file")
-def main(inital_amount, orders_file, out_file):
+import QSTK.qstkutil.tsutil as tsu
+import pandas as pd
+import numpy as np
+from math import sqrt
+
+from simulate import process_orders, get_close_prices
+
+def similate_portfolio(initial_amount, df_orders, custom_enddate=None):
     """
     Load the orders_file as csv.
     Convert it and the initia_amount to dict_orders.
     Call simulate with the dict.
     Write the results to out-file.
     """
+    df_orders.columns = ["year","month","day","symbol","bs","amount","xx"]
+    for c in ["symbol","bs"]:
+        df_orders[c] = df_orders[c].map(str.strip)
+
+    df_orders["amount"] = np.where(df_orders["bs"].str.upper() =="SELL", (df_orders["amount"] * -1), df_orders["amount"])
+    ts_dates = pd.to_datetime((df_orders.year*10000+df_orders.month*100+df_orders.day).apply(str),format='%Y%m%d')
+    df = df_orders[["symbol","amount"]]
+    df = df.pivot(columns='symbol', values='amount')
+    df["date"]=ts_dates
+   
+    df_result = process_orders(df, float(initial_amount))
+    return df_result
+
+@click.command()
+@click.argument("initial_amount")
+@click.argument("orders_file", type=click.Path(exists=True))
+@click.argument("out_file")
+def main(initial_amount, orders_file, out_file):
+    df_orders = pd.read_csv(orders_file, header=None)
+    df_portfolio = similate_portfolio(initial_amount, df_orders)
+    start_date = df_portfolio.index[0]
+    end_date = df_portfolio.index[-1]
     
+    df_portfolio[["year","month","day","portfolio"]].to_csv(out_file,header=False,index=False)
+    spx_prices = get_close_prices(start_date, end_date, ["$SPX"])["$SPX"].values
+    spx_ret = tsu.returnize0(spx_prices.copy())
+    p_ret = tsu.returnize0(df_portfolio["portfolio"].values.copy())
+    print "Details of the Performance of the portfolio :"
+    print "Data Range :  %s  to  %s\n" % (start_date, end_date)
+    print "Sharpe Ratio of Fund : %s" % ((sqrt(252) * p_ret.mean()/p_ret.std()))
+    print "Sharpe Ratio of $SPX : %s\n" %((sqrt(252) * spx_ret.mean()/spx_ret.std()))
+    print "Total Return of Fund :  %s" % (df_portfolio["portfolio"][-1]/df_portfolio["portfolio"][0])
+    print "Total Return of $SPX : %s\n" %(spx_prices[-1]/spx_prices[0])
+    print "Standard Deviation of Fund :  %s" % (p_ret.std())
+    print "Standard Deviation of $SPX : %s\n" % (spx_ret.std())
+    print"Average Daily Return of Fund :  %s" % (p_ret.mean())
+    print "Average Daily Return of $SPX : %s" % (spx_ret.mean())
+
 if "__main__" in __name__:
     main()
--- computation_investing/quizes/market_sim/simulate.py
+++ computation_investing/quizes/market_sim/simulate.py
@@ -5,6 +5,7 @@ import numpy as np
 from datetime import datetime
 from datetime import timedelta
 from operator import itemgetter
+import logging
 import pandas as pd
 import datetime as dt
 import QSTK.qstkutil.qsdateutil as du
@@ -26,33 +27,33 @@ def ignore_exception(IgnoreException=Exception,DefaultVal=None):
     return dec
 
     
-def get_next_open_price(prices, date, symbol):
-    open_prices = prices["open"]
-    any_matches = lambda : open_prices[open_prices.index >date].shape[0] > 0
-    if any_matches():
-        return float(open_prices[open_prices.index >date].iloc[0][symbol])
-    
-    return None
+def get_close_prices(dt_start,dt_end, ls_symbols):
+    dt_timeofday = dt.timedelta(hours=16)
+    ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt_timeofday)
+    dataobj = da.DataAccess("Yahoo", verbose=False)
+    data = dataobj.get_data(ldt_timestamps, ls_symbols, ["close"])
+    return data[0]
 
-def process_orders(orders, initial_cash=10000):
+def process_orders(df_orders, initial_cash=10000,custom_enddate=None):
+    if not isinstance(initial_cash, float):
+        print ("initial_case must be a float")
+        return None
+    if not isinstance(df_orders, pd.DataFrame):
+        print ("df_orders must be a DataFrame")
+        return None
     add_16h = lambda dt: dt + timedelta(hours=16) 
-    #orders  = sorted(orders, key=itemgetter("date"))
-    orders["date"] = map(add_16h, orders["date"])
-    df_orders=pd.DataFrame(orders)
+    df_orders["date"] = map(add_16h, df_orders["date"])
     df_orders = df_orders.set_index("date")
-    #ls_symbols = list(set([x["symbol"] for x in orders if not "PORTFOLIO" in x["symbol"]]))
     ls_symbols = list(df_orders.columns.values)
-    #ls_symbols = np.loadtxt("/mnt/extradrive1/projects/market_makers/computation_investing/quizes/event_study/short.txt",dtype='S10',comments='#')
-    dt_start = df_orders.index[0] + dt.timedelta(days=-1)
-    dt_end = df_orders.index[-1]
-    #dt_start = min([x["date"] for x in orders]) - timedelta(days=1)
-    #dt_end = max([x["date"] for x in orders]) + timedelta(days=1)
-    #dt_start = dt.datetime(int(2008) - 1,1,1)
-    #dt_end = dt.datetime(int(2008),12,31)
+    dt_start = df_orders.index[0] # + dt.timedelta(days=-1)
+    dt_end = df_orders.index[-1] if custom_enddate is None else custom_enddate
     ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt.timedelta(hours=16))
-    dataobj = da.DataAccess("Yahoo", verbose=True)
+    after_hrs_order_dates = np.setdiff1d(df_orders.index.tolist(), ldt_timestamps)
+    if after_hrs_order_dates:
+        ldt_timestamps.extend(after_hrs_order_dates)
+    dataobj = da.DataAccess("Yahoo", verbose=False)
     ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
-    ldf_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys, verbose=True)
+    ldf_data = dataobj.get_data(ldt_timestamps, ls_symbols, ls_keys, verbose=False)
     for d in ldf_data:
         d.fillna(method="pad")
         d.fillna(method="bfill")
@@ -61,9 +62,9 @@ def process_orders(orders, initial_cash=10000):
     df_zeros = pd.DataFrame(0,ldt_timestamps, columns=ls_symbols)
     # drop any orders not on trading days
     df_order_wrong_day = df_orders[~df_orders.index.isin(df_zeros.index)]
-    print ("Dropping Orders on Market Closed Days...")
-    print (df_order_wrong_day)
-    df_orders.drop(df_order_wrong_day.index, inplace=True)
+    #print ("Dropping Orders on Market Closed Days...")
+    #print (df_order_wrong_day)
+    #df_orders.drop(df_order_wrong_day.index, inplace=True)
     # create a data frame with rows for each date in range
     # containing the trade values
     df_order_trades = df_orders.combine_first(df_zeros)
@@ -72,25 +73,19 @@ def process_orders(orders, initial_cash=10000):
     # Calculate the porfolio stock holdings for each day
     df_portfolio_holdings = df_order_trades.cumsum(axis=0)
     # Calculate the cash portion of the portfolio
-    df_portfolio_cash = ((df_trade_values.sum(axis=1) * -1) + initial_cash).cumsum()
+    df_portfolio_cash = initial_cash - (df_trade_values.sum(axis=1).cumsum())
     # Calcuate the cash value of the portfolio stock holdings
-    df_portfolio_stock_value = df_portfolio_holdings * df_prices_close
+    df_portfolio_stocks_value = df_portfolio_holdings * df_prices_close
+    df_portfolio_holdings_value = df_portfolio_stocks_value.sum(axis=1)
+
     # Calculate the daily portfolio value (sum of stock values and cash)
-    df_portfolio_value = df_portfolio_stock_value.sum() + df_portfolio_cash
-    #for order in orders:
-    #    if "DEPOSIT" in order["action"]:
-    #        print 'make deposit: $%s to %s' % (order["shares"],order["symbol"])
-    #    if "BUY" in order["action"]:
-    #        price =  get_next_open_price(d_data, order["date"], order["symbol"])
-    #        print 'BUY %s, %s @%s' % (order["shares"], order["symbol"],price)
-    #    if "SELL" idn order["action"]:
-    #        price =  get_next_open_price(d_data, order["date"], order["symbol"])
-    #        print 'SELL %s, %s, @%s' % (order["shares"], order["symbol"], price)
-    return "result"   
-    #print "multi-pass portfolio: %s " % (orders)
-class Portfolio(object):
-    stocks=None
-    cash=0
+    df_portfolio_value = df_portfolio_holdings_value + df_portfolio_cash
+    df_portfolio_value.name = "portfolio"
+    df_ym = pd.DataFrame([{"year":x.year,"month":x.month, "day":x.day} \
+        for x in df_portfolio_holdings.index.tolist()], index=df_portfolio_holdings.index)
+    df_portfolio_value = df_ym.join(df_portfolio_value)
+    return df_portfolio_value
+
 @click.command()
 def main():
     orders = []
